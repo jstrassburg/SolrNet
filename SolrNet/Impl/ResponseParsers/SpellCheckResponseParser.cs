4765c975bba85e8504d06f63de1e4e2b352b1f4d
JiM Strassburg <jstrassburg@directs.com>
2016-03-25 14:38:14 -0500

    Integrating PR 194 which has support for the new spellcheck response in Solr 5 w/backwards compatibility to the old format

--- SolrNet/Impl/ResponseParsers/SpellCheckResponseParser.cs
+++ SolrNet/Impl/ResponseParsers/SpellCheckResponseParser.cs
@@ -20,18 +20,22 @@ using System.Xml.Linq;
 using System.Xml.XPath;
 using SolrNet.Utils;
 
-namespace SolrNet.Impl.ResponseParsers {
+namespace SolrNet.Impl.ResponseParsers
+{
     /// <summary>
     /// Parses spell-checking results from a query response
     /// </summary>
     /// <typeparam name="T">Document type</typeparam>
-    public class SpellCheckResponseParser<T> : ISolrResponseParser<T> {
-        public void Parse(XDocument xml, AbstractSolrQueryResults<T> results) {
+    public class SpellCheckResponseParser<T> : ISolrResponseParser<T>
+    {
+        public void Parse(XDocument xml, AbstractSolrQueryResults<T> results)
+        {
             results.Switch(query: r => Parse(xml, r),
                            moreLikeThis: F.DoNothing);
         }
 
-        public void Parse(XDocument xml, SolrQueryResults<T> results) {
+        public void Parse(XDocument xml, SolrQueryResults<T> results)
+        {
             var spellCheckingNode = xml.XPathSelectElement("response/lst[@name='spellcheck']");
             if (spellCheckingNode != null)
                 results.SpellChecking = ParseSpellChecking(spellCheckingNode);
@@ -42,14 +46,26 @@ namespace SolrNet.Impl.ResponseParsers {
         /// </summary>
         /// <param name="node"></param>
         /// <returns></returns>
-        public SpellCheckResults ParseSpellChecking(XElement node) {
+        public SpellCheckResults ParseSpellChecking(XElement node)
+        {
             var r = new SpellCheckResults();
             var suggestionsNode = node.XPathSelectElement("lst[@name='suggestions']");
-            var collationNode = suggestionsNode.XPathSelectElement("str[@name='collation']");
-            if (collationNode != null)
-                r.Collation = collationNode.Value;
+            IEnumerable<XElement> collationNodes;
+
+            // Solr 5.0+
+            var collationsNode = node.XPathSelectElement("lst[@name='collations']");
+            if (collationsNode != null)
+                collationNodes = collationsNode.XPathSelectElements("str[@name='collation']");
+            // Solr 4.x and lower
+            else
+                collationNodes = suggestionsNode.XPathSelectElements("str[@name='collation']");
+
+            foreach (var cn in collationNodes)
+                r.Collations.Add(cn.Value);
+
             var spellChecks = suggestionsNode.Elements("lst");
-            foreach (var c in spellChecks) {
+            foreach (var c in spellChecks)
+            {
                 var result = new SpellCheckResult();
                 result.Query = c.Attribute("name").Value;
                 result.NumFound = Convert.ToInt32(c.XPathSelectElement("int[@name='numFound']").Value);
@@ -57,7 +73,8 @@ namespace SolrNet.Impl.ResponseParsers {
                 result.StartOffset = Convert.ToInt32(c.XPathSelectElement("int[@name='startOffset']").Value);
                 var suggestions = new List<string>();
                 var suggestionNodes = c.XPathSelectElements("arr[@name='suggestion']/str");
-                foreach (var suggestionNode in suggestionNodes) {
+                foreach (var suggestionNode in suggestionNodes)
+                {
                     suggestions.Add(suggestionNode.Value);
                 }
                 result.Suggestions = suggestions;
